480b6000e7613ee4154fd764c8addd190d4713f0
Germain <germain.gau@gmail.com>
2016-07-20 19:04:36 +0200

    progress bar on hash computing

--- main.py
+++ main.py
@@ -1,20 +1,27 @@
 from functions import *
 from scipy.optimize import curve_fit
+from os.path import abspath
+from tqdm import tqdm
 
-print("loading the video...")
-cap = cv2.VideoCapture("video_test.mp4")
+VIDEO_PATH = abspath("video_test.mp4")
+print("[INIT] Loading video at", VIDEO_PATH)
+
+cap = cv2.VideoCapture(VIDEO_PATH)
 while not cap.isOpened():
-    cap = cv2.VideoCapture("video_test.mp4")
+    cap = cv2.VideoCapture(VIDEO_PATH)
 
+VIDEO_FRAMES_COUNT = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
 
 print("computing hashes...")
 L = []
 
-while (cap.isOpened()):
-    ret, frame = cap.read()
-    gray = cv2cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    L.append(phash64(gray))
-    print(phash64(gray))
+with tqdm(total=VIDEO_FRAMES_COUNT) as pbar:
+    while (cap.isOpened()):
+        ret, frame = cap.read(0) # 0 flag for an already greyscaled image
+        # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        phash = phash64(frame)
+        L.append(phash)
+        pbar.update()
 
 cap.release()
 
@@ -34,12 +41,8 @@ plt.show()
 plt.hist(LL, bins=max(d.keys()))
 plt.show()
 
-def func(x, a, b, c):
-    return a * np.exp(-b * x) + c
-
 nb_bins = max(d.keys())
 h, _, _ = plt.hist(LL, bins=nb_bins)
 
-popt, pcov = curve_fit(func, np.array(list(range(1, nb_bins + 1))), h)
-
-
+popt, pcov = curve_fit(lambda x, a, b, c: a * np.exp(-b * x) + c, \
+                np.array(list(range(1, nb_bins + 1))), h)
