59d75d467bd0b3c8f24aa4b0d2924f45c8709650
Germain <germain.gau@gmail.com>
2016-07-20 23:45:26 +0200

    added src volume

--- run.sh
+++ run.sh
@@ -1,11 +1,10 @@
 if [ "$1" != "cache" ]; then
     echo "For not building again and again write :"
     echo "$ ./run.sh cache"
-    
+
     #build
     docker build -t scd .
 fi
 
 #run with videos in the volume...
-docker run -v $(pwd)/videos:/src/videos scd 
-
+docker run -v $(pwd)/src:/src -v $(pwd)/videos:/src/videos scd 
