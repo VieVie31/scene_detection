17246d2316ee6d31f983c3da7c0718c1714472c9
Germain <germain.gau@gmail.com>
2016-07-20 23:45:59 +0200

    arg parsing + multiple files

--- src/main.py
+++ src/main.py
@@ -2,47 +2,51 @@ from functions import *
 from scipy.optimize import curve_fit
 from os.path import abspath
 from tqdm import tqdm
+from sys import argv
 
-VIDEO_PATH = abspath("videos/video_test.mp4")
-print("[INIT] Loading video at", VIDEO_PATH)
+if __name__ == '__main__':
+    # Temporary arg parsing
+    for source in argv[1:]:
+        print('Hashing', source)
+        VIDEO_PATH = abspath(source)
+        print("[INIT] Loading video at", VIDEO_PATH)
 
-cap = cv2.VideoCapture(VIDEO_PATH)
-while not cap.isOpened():
-    cap = cv2.VideoCapture(VIDEO_PATH)
+        cap = cv2.VideoCapture(VIDEO_PATH)
+        while not cap.isOpened():
+            cap = cv2.VideoCapture(VIDEO_PATH)
 
-VIDEO_FRAMES_COUNT = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+        VIDEO_FRAMES_COUNT = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
 
-print("computing hashes...")
-L = []
+        print("computing hashes...")
+        L = []
 
-with tqdm(total=VIDEO_FRAMES_COUNT) as pbar:
-    while (cap.isOpened()):
-        ret, frame = cap.read(0) # 0 flag for an already greyscaled image
-        # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        phash = phash64(frame)
-        L.append(phash)
-        pbar.update()
+        with tqdm(total=VIDEO_FRAMES_COUNT) as pbar:
+            while (cap.isOpened()):
+                ret, frame = cap.read(0) # 0 flag for an already greyscaled image
+                # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                phash = phash64(frame)
+                L.append(phash)
+                pbar.update()
 
-cap.release()
-
-exit()
-
-print("computing inter frames difference...")
-LL = [hamming(L[i - 1], L[i]) for i in range(1, len(L))]
-
-d = histogram(LL)
-sigma = variance(d)**.5
-
-#display the distance between 2 frames during the video
-plt.plot(LL)
-plt.show()
-
-#display the repartition of inter frames distance
-plt.hist(LL, bins=max(d.keys()))
-plt.show()
-
-nb_bins = max(d.keys())
-h, _, _ = plt.hist(LL, bins=nb_bins)
-
-popt, pcov = curve_fit(lambda x, a, b, c: a * np.exp(-b * x) + c, \
-                np.array(list(range(1, nb_bins + 1))), h)
+        cap.release()
+        # exit()
+        #
+        # print("computing inter frames difference...")
+        # LL = [hamming(L[i - 1], L[i]) for i in range(1, len(L))]
+        #
+        # d = histogram(LL)
+        # sigma = variance(d)**.5
+        #
+        # #display the distance between 2 frames during the video
+        # plt.plot(LL)
+        # plt.show()
+        #
+        # #display the repartition of inter frames distance
+        # plt.hist(LL, bins=max(d.keys()))
+        # plt.show()
+        #
+        # nb_bins = max(d.keys())
+        # h, _, _ = plt.hist(LL, bins=nb_bins)
+        #
+        # popt, pcov = curve_fit(lambda x, a, b, c: a * np.exp(-b * x) + c, \
+        #                 np.array(list(range(1, nb_bins + 1))), h)
