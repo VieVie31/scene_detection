31d0ba3c4e984f7a7848237671dae43d5fe2b781
Germain <germain.gau@gmail.com>
2016-07-21 16:55:25 +0200

    ignore already encoded videos

--- src/main.sh
+++ src/main.sh
@@ -4,8 +4,12 @@ echo "Converting videos"
 cd videos
 for f in *;
 do
-  echo "Encoding $f"
-  ffmpeg -i $f -s 20x20 -vf hue=s=0 -an -r 10 encoded_$f
+  if grep -q 'encoded' "$f"; then
+    echo 'Skipping already encoded video.'
+  else
+    echo "Encoding $f"
+    ffmpeg -i $f -s 20x20 -vf hue=s=0 -an -r 10 encoded_$f
+fi
 done
 
 echo "Running hasher"
