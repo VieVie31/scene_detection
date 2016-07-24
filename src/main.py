3d126ff5c39575502d3842433298a3e4c59a60d6
Germain <germain.gau@gmail.com>
2016-07-24 12:13:03 +0200

    added frames differences export on graph + caching possibilities

--- src/main.py
+++ src/main.py
@@ -1,25 +1,61 @@
-from functions import dhash
+from functions import dhash, hamming, dhash_freesize
 from os.path import abspath
-from tqdm import tqdm
+from tqdm import tqdm, trange
 from sys import argv
-
-import skvideo.io
+from collections import defaultdict
+from pprint import pformat
+from itertools import combinations
+from skvideo.io import ffprobe, vreader
+import os
+import warnings
+# Remove matplotlib warning because of fc-cache building
+with warnings.catch_warnings():
+    warnings.simplefilter("ignore")
+    import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
+    stats = []
     # Temporary arg parsing
-    for source in argv[1:]:
-        print('Hashing', source)
+    for _, source in zip(trange( \
+                            len(argv) - 1, \
+                            desc='Files', \
+                            ), \
+                         argv[1:]):
+
+        # Loading source video
         VIDEO_PATH = abspath(source)
-        cap = skvideo.io.vreader(VIDEO_PATH)
-#        VIDEO_FRAMES_COUNT = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+        METADATA = ffprobe(source)['video']
+        VIDEO_FRAMES_COUNT = int(METADATA["@nb_frames"])
+        cap = vreader(VIDEO_PATH)
 
-        print("computing hashes...")
         L = []
+        diffs = []
 
-        with tqdm() as pbar:
-            for frame in cap:
-                phash = dhash(frame)
-                print(phash)
-                L.append(phash)
-                pbar.update()
+        # Hashing
+        count_tab = defaultdict(lambda: 0, {})
+        for _, frame in zip(trange(VIDEO_FRAMES_COUNT, leave=False), cap):
+            hash_img = int(dhash_freesize(frame))
+            count_tab[hash_img] += 1
+            if len(L):
+                diffs += [hamming(hash_img, L[-1])]
+            else:
+                diffs += [0]
+            L.append(hash_img)
 
+        collisions = sum(count_tab.values()) - len(count_tab)
+        assert len(L) == VIDEO_FRAMES_COUNT
+        stats += [{
+            'source': VIDEO_PATH,
+            'frames': VIDEO_FRAMES_COUNT,
+            'collisions': collisions,
+            'datas': {
+                'diffs': diffs,
+                'hashes': L
+            }
+        }]
+
+        # Creating output graph
+        plt.plot(L)
+        plotfile = '/videos/%s.png' % source.split('/')[-1]
+        plt.savefig(plotfile)
+    tqdm.write(pformat(stats, indent=2, depth=2))
--- src/main.sh
+++ src/main.sh
@@ -1,17 +1,50 @@
 #! /bin/sh
 
+# created files prefix
+export PREFIX=out_
+SOURCE_DIRECTORY=/videos
+
+# Set to /videos to store ffmpeg encoded videos on fs or something else
+# for temp folder usage
+DESTINATION_DIRECTORY=/videos
+
+
 echo "Converting videos"
 # creating tmp encoded datas destination folder
 # /!\ Removed when container exits
-mkdir /datas
+mkdir -p $DESTINATION_DIRECTORY
 
-cd videos
+cd $SOURCE_DIRECTORY
+
+# remove old png exports, ensuring analysis won't try to use them as videos
+rm -vf $PREFIX*.png
+
+current=1
+# Ignore previous analysis traces in total
+((total = $(ls $SOURCE_DIRECTORY | wc -l) - $(ls $PREFIX* 2>/dev/null | wc -l)))
+
 for f in *;
 do
-  echo "Encoding $f"
-  ffmpeg -i $f -s 20x20 -vf hue=s=0 -an -r 5 /datas/$f
+  # Does not reencode cached output from a previous analysis
+  if [[ $f == $PREFIX* ]];
+  then
+    continue
+  fi
+
+  echo "[$current / $total] $f"
+  # increment file counter
+  ((current++))
+  ffmpeg  -loglevel warning \
+          -stats \
+          -i "$f" \
+          -s 8x8 \
+          -vf hue=s=0 \
+          -an \
+          -r 1 \
+          "$DESTINATION_DIRECTORY/$PREFIX$f"
+
 done
+cd /
 
 echo "Running hasher"
-ls /datas
-python3.5 -u /src/main.py /datas/*
+python3.5 -u /src/main.py $DESTINATION_DIRECTORY/$PREFIX*
