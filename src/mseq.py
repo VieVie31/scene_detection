916b0f00163c8dc6135d3d42458d15bb2cfc95f4
VieVie31 <orissermaroix@gmail.com>
2017-03-12 17:06:45 +0100

    A parser has been added to the mseq file to allow tests form command line...

--- src/mseq.py
+++ src/mseq.py
@@ -1,49 +1,72 @@
+#this file is not a module to import but contains some tests
+#about the pattern discovery functions created for the scd project
+if __name__ != "__main__":
+    exit(42)
+
 import math
 import random
+import argparse
 import numpy as np
 
-import warnings
+#import warnings
 #remove matplotlib warning because of fc-cache building
-with warnings.catch_warnings():
-    warnings.simplefilter("ignore")
-    import matplotlib.mlab as mlab
-    import matplotlib.pyplot as plt
+#with warnings.catch_warnings():
+#    warnings.simplefilter("ignore")
+#    import matplotlib.mlab as mlab
+#    import matplotlib.pyplot as plt
 
 from collections import Counter
 from pprint import pprint
+from sys import argv
 
 #importing my set of functions
 from functions import *
 
+#create a parser to automatise some tests
+parser = argparse.ArgumentParser(
+        prog="mseq", 
+        description='Test file used to benchmark different method for the generic finding problem.')
+#add command to the parser
+parser.add_argument('-bm',  '--bound-max',       type=int, help='The random numbers will uniformly be selected between 0 and bound-max.', default=2**8)
+parser.add_argument('-tl',  '--total-length',    type=int, help='The length of the list containing random numbers.', default=10000)
+parser.add_argument('-sl',  '--sequence-length', type=int, help='The length of the random pattern to insert.', default=10)
+parser.add_argument('-ni',  '--nb-insert',       type=int, help='How many time the random pattern gotta be inserted randomly in the long initial sequence.', default=12)
+parser.add_argument('-std', '--nb-std',          type=int, help='The number of standard deviation to consider a tri-gram could be part of the pattern.', default=4)
 
-BOUND_MAX = 2**8
-L_LENGTH = 10000
-SL_LENGTH = 10
-NB_INSERT = 12
-NB_STD = 4
+#parse the arguments
+args = parser.parse_args(argv[1:])
 
-print("generation...")
+BOUND_MAX = args.bound_max
+L_LENGTH  = args.total_length
+SL_LENGTH = args.sequence_length
+NB_INSERT = args.nb_insert
+NB_STD    = args.nb_std
 
+print("bound_max,total_length,sequence_length,nb_insert,nb_std,sequence_length_lzw,hamming_distance_lzw_origin,sequence_length_stat,hamming_distance_stat_origin")
+
+#generating the random sequence and pattern
 L   = [random.randrange(0, BOUND_MAX) for i in range(L_LENGTH)]  #the long sequence
 SL  = [random.randrange(0, BOUND_MAX) for i in range(SL_LENGTH)] #the shorter sequence
 idx = [random.randrange(0, L_LENGTH) for i in range(NB_INSERT)]  #index in the lonsequence to appen the shorter
 idx.sort()
 
-
+#insert the pattern
 for i in idx:
     L = L[:i] + SL + L[i:]
-
-NB_STD = 4
 
 ##f = open("/Users/mac/Desktop/scene_detection/cache/L.txt", 'r')
 ##L = eval(f.read())
 ##f.close()
 
-print("sequence solution : ", end='')
-print(SL)
-print("sequence hash of hashes : ", end='')
+print("{},{},{},{},{},".format(
+    BOUND_MAX,
+    L_LENGTH,
+    SL_LENGTH,
+    SL_LENGTH,
+    SL_LENGTH), end='')
+
+#compute the hash of the inserted sequence
 h_o_h = get_hash_of_hashes(SL)
-print(h_o_h)
 
 
 ################################
@@ -57,89 +80,21 @@ def index_of_sublist_in_list(lst, sub_lst):
     raise ValueError("Sub list not found !! :'(")
 
 reconstitued_sequence = cseq(L)
- 
-print("LZW approcah...")
-print(reconstitued_sequence)
-h_o_lzw = get_hash_of_hashes(reconstitued_sequence)
-print("hash of hahses : ", end='')
-print(h_o_lzw)
-print("hamming distance between hashes : ", end='')
-print(hamming(h_o_h, h_o_lzw))
-print("found exactly the same sequence : ", reconstitued_sequence == SL)
+
+print("{},{},".format(
+    len(reconstitued_sequence),
+    hamming(h_o_h, get_hash_of_hashes(reconstitued_sequence))), end='')
 
 ############################
 # THE STATISTICAL APPROCAH #
 ############################
 
-print("searching...")
+reconstitued_sequence = mseq(L, nb_std=NB_STD)
+print("{},{}".format(
+    len(reconstitued_sequence),
+    hamming(h_o_h, get_hash_of_hashes(reconstitued_sequence))), end='')
 
-print(len(L))
-
-NB_STD = 4
-
-d = {}
-for i in range(len(L) - 2): #-1):#- 2):
-    tpl = ((L[i], L[i + 1]), L[i + 2]) #(L[i], L[i + 1]) #((L[i], L[i + 1]), L[i + 2])
-    if tpl in d:
-        d[tpl] += 1
-    else:
-        d[tpl] = 1
+print('')
 
 
-a = list(sorted(list(d.values())))
-a = np.array(a)
 
-
-#keep number bigger than 6 standard deviation (cf. anomally detection)
-#keep keys in d that have a counter bigger than NB_STD * std
-dd = {}
-for k in d:
-    if d[k] >= a.mean() + NB_STD * a.std():
-        dd[k] = d[k]
-
-#remap and keep the keys
-dd = {(a, b, c) : [] for ((a, b), c) in dd.keys()}
-
-#create all possibles paths
-for (a, b, c) in dd:
-    dd[(a, b, c)] = filter(lambda t: (b, c) == (t[0], t[1]), dd.keys())
-
-#get the longest path without cycle --> should be the SL (or close to, statistically)
-longest = []
-
-visited = set()
-path = []
-def search(v, d={}):
-    visited.add(v)
-    path.append(v)
-    
-    if len(path) > len(longest):
-        longest[:] = path[:]
-        
-    for w in d[v]:
-        if w not in visited:
-            search(w, d)
-            
-    path.pop()
-    visited.remove(v)
-
-#try all possibilities
-for v in dd:
-    search(v, dd)
-
-#remake the list of longest
-L = list(longest[0])
-for t in longest[1:]:
-    L.append(t[2])
-
-print("statistical approach...")
-print("reconstructed sequence : ", end='')
-#print(L == SL)
-print(L)
-#print(SL)
-
-h_o_s = get_hash_of_hashes(L)
-print("reconstitued sequence hash of hashes : ", end='')
-print(h_o_s)
-print("distance between hashes : ", end='')
-print(hamming(h_o_h, h_o_s))
